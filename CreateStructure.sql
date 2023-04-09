fdd53020953f183670ea9c6ac5a3f05c481323b7
U1demir <vovachal39@gmail.com>
2023-04-09 22:26:09 +0200

    Правки. Изменен тип dbo.Basket.DiscountValue на decimal и прочие незначительные корректировки

--- CreateStructure.sql
+++ CreateStructure.sql
@@ -1,30 +1,36 @@
---Таблица складского учета SKU
+-- Таблица складского учета SKU
+
 create table dbo.SKU (
   ID int identity (1, 1) not null primary key,
   Code as concat('s', ID) unique,
   Name varchar(255) not null
 );
 
---Таблица покупателей
+
+-- Таблица покупателей
+
 create table dbo.Family (
   ID int identity (1, 1) not null primary key,
   Surname varchar(255) not null,
-  BudgetValue decimal not null, 
+  BudgetValue decimal (18, 2) not null, 
+ -- Добавил условие 
   constraint CHK_BudgetValue check(BudgetValue >= 0)
 );
 
---Корзина
+
+-- Корзина
+
 create table dbo.Basket (
   ID int identity (1, 1) not null primary key,
   ID_SKU int not null,
   ID_Family int not null,
   Quantity int not null,
-  Value decimal not null,
+  Value decimal (18, 2) not null,
   PurchaseDate date not null default getdate(),
-  DiscountValue int not null default 0,
+  DiscountValue decimal (18, 2) not null default 0,
   constraint FK_ID_SKU foreign key(ID_SKU)
     references dbo.SKU(ID)
-	-- Условия ON опциональны, применил NO ACTION как условие по умолчанию 
+	-- Условия ON опциональны, применил NO ACTION 
 	on delete no action
 	on update no action,
   constraint FK_ID_Family foreign key(ID_Family)
