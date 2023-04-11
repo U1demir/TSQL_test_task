bb7226258132c41c727fe00494f59a1f1178718e
U1demir <vovachal39@gmail.com>
2023-04-11 23:22:36 +0200

    Исправлен триггер dbo.TR_Basket_insert_update

--- dbo.TR_Basket_insert_update.sql
+++ dbo.TR_Basket_insert_update.sql
@@ -2,9 +2,11 @@
 
 create trigger dbo.TR_Basket_insert_update on dbo.Basket
 for insert, update as
-if exists (select ID_SKU from inserted group by ID_SKU having count(*) >= 2) 
-	begin
-		update dbo.Basket
-		set DiscountValue = dbo.Basket.Value * 0.05 
-		from dbo.Basket inner join (select ID_SKU from inserted group by ID_SKU having count(*) >= 2) as i on dbo.Basket.ID_SKU = i.ID_SKU
-	end;
+begin
+	update dbo.Basket
+	set DiscountValue = case 
+	when @@ROWCOUNT > 0 then dbo.Basket.Value * 0.05 
+	else 0.00	
+	end
+	from dbo.Basket inner join inserted as i1 on dbo.Basket.ID = i1.ID inner join inserted as i2 on i1.ID_SKU = i2.ID_SKU where i1.ID <> i2.ID
+end;
