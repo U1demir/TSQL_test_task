0e1301bb11d980bf4283b58722104ace7d539f01
U1demir <vovachal39@gmail.com>
2023-04-11 21:20:28 +0200

    Исправление п.1 функция declare @result decimal(18, 2)

--- dbo.udf_GetSKUPrice.sql
+++ dbo.udf_GetSKUPrice.sql
@@ -3,8 +3,7 @@
 create function dbo.udf_GetSKUPrice (@ID_SKU as int)
 	returns decimal(18, 2)
 	begin
-		declare @value decimal(18, 2), @qty int
-		select @value = sum(Value) from dbo.Basket where ID_SKU = @ID_SKU
-		select @qty = sum(Quantity) from dbo.Basket where ID_SKU = @ID_SKU
-		return @value / @qty
+		declare @result decimal(18, 2)
+		select @result = sum(dbo.Basket.Value)/sum(dbo.Basket.Quantity) from dbo.Basket where ID_SKU = @ID_SKU		
+		return @result
 	end;
