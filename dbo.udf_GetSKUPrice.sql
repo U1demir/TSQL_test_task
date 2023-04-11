3f88156ef40dec8d9e03d21901500a6258d1234d
U1demir <vovachal39@gmail.com>
2023-04-11 21:33:13 +0200

    Исправление функции dbo.udf_GetSKUPrice

--- dbo.udf_GetSKUPrice.sql
+++ dbo.udf_GetSKUPrice.sql
@@ -1,9 +1,10 @@
--- Функция рассчитывает стоимость передаваемого продукта из таблицы dbo.Basket по формуле
+-- Функция рассчитывает стоимость передаваемого продукта из таблицы dbo.Basket по формуле.
 
 create function dbo.udf_GetSKUPrice (@ID_SKU as int)
 	returns decimal(18, 2)
 	begin
-		declare @result decimal(18, 2)
-		select @result = sum(dbo.Basket.Value)/sum(dbo.Basket.Quantity) from dbo.Basket where ID_SKU = @ID_SKU		
-		return @result
+		declare @value decimal(18, 2), @qty int
+		select @value = sum(Value) from dbo.Basket where ID_SKU = @ID_SKU
+		select @qty = sum(Quantity) from dbo.Basket where ID_SKU = @ID_SKU
+		return @value / @qty
 	end;
