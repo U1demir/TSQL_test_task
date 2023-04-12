7b299896b7ab05655202bd42a68ed7d47249ca2c
U1demir <vovachal39@gmail.com>
2023-04-12 15:04:51 +0200

    Незначительные правки по форматированию

--- dbo.TR_Basket_insert_update.sql
+++ dbo.TR_Basket_insert_update.sql
@@ -6,7 +6,10 @@ begin
 	update dbo.Basket
 	set DiscountValue = case 
 	when @@ROWCOUNT > 0 then dbo.Basket.Value * 0.05 
-	else 0.00	
+	else 0.00 
 	end
-	from dbo.Basket inner join inserted as i1 on dbo.Basket.ID = i1.ID inner join inserted as i2 on i1.ID_SKU = i2.ID_SKU where i1.ID <> i2.ID
+	from dbo.Basket 
+		inner join inserted as i1 on dbo.Basket.ID = i1.ID 
+		inner join inserted as i2 on i1.ID_SKU = i2.ID_SKU 
+		where i1.ID <> i2.ID
 end;
