61ea3b9a5400bd4650057e10a0d01a95d6ef0b5e
U1demir <vovachal39@gmail.com>
2023-04-14 14:49:30 +0200

    Исправления триггера TR_Basket_insert_update v.2

--- dbo.TR_Basket_insert_update.sql
+++ dbo.TR_Basket_insert_update.sql
@@ -3,13 +3,32 @@
 create trigger dbo.TR_Basket_insert_update on dbo.Basket
 for insert, update as
 begin
+	with cte_set as (
+	select 
+		ID_SKU
+		,count(*) as row_count
+	from inserted
+	group by 
+		ID_SKU
+	having count(*) >= 2
+	)
 	update dbo.Basket
-	set DiscountValue = case 
-	when @@rowcount > 0 then dbo.Basket.Value * 0.05 
-	else 0.00 
-	end
+	set DiscountValue = dbo.Basket.Value * 0.05
+	from dbo.Basket inner join cte_set on dbo.Basket.ID_SKU = cte_set.ID_SKU;
+
+	-- Обработка одиночных SKU
+	with cte_single as (
+	select 
+		ID_SKU
+		,count(*) as row_count
+	from inserted
+	group by
+		ID_SKU
+	having count(*) = 1
+	)	
+	update dbo.Basket
+	set DiscountValue = 0.00	
 	from dbo.Basket 
-		inner join inserted as i1 on dbo.Basket.ID = i1.ID 
-		inner join inserted as i2 on i1.ID_SKU = i2.ID_SKU 
-	where i1.ID <> i2.ID
+		inner join cte_single on dbo.Basket.ID_SKU = cte_single.ID_SKU 
+		inner join inserted on dbo.Basket.ID = inserted.ID
 end;
